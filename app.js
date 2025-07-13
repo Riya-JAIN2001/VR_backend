26de466646d8df946244df0a64a6c57d0930eb46
Riya <167556137+Riya-JAIN2001@users.noreply.github.com>
2025-07-13 21:26:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-  origin: '*', // allows all origins
+  origin: 'https://vr-automation-frontend.vercel.app', // allows all origins
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 })
 );
